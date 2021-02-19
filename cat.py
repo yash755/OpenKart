f6ecbd45feb282d92f02278e0adde79672e0b22c
yash gupta <yash.gupta755@gmail.com>
2021-02-20 00:28:07 +0530

    first commit

---
+++ cat.py
@@ -0,0 +1,45 @@
+import pymysql.cursors
+
+
+def get_list():
+    try:
+        connection = pymysql.connect(host="dock14.com",
+                                     user="dockonef_jashg",
+                                     passwd="1uWSEW#pcZcJ",
+                                     db="dockonef_scrape",
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM adi_category")
+                connection.commit()
+
+                for row in cursor:
+                    try:
+                        data = row
+                        pageCount = data['id']
+                        category = data['category']
+                        pageURL = data['pageurl']
+                        subCategory = data['subcategory']
+
+                        print (category)
+
+
+                    except Exception as e:
+                        print ("Loop3")
+                        print (e)
+                        continue
+        except Exception as e:
+            print ('Failed Query')
+            print ("Loop2")
+            print (e)
+    except pymysql.Error as e:
+        print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+        print ("Loop1")
+        time.sleep(2)
+        print("Was a nice sleep, now let me continue...")
+
+
+if __name__ == '__main__':
+    while True:
+        get_list()
