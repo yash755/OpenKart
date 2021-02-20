2a9fe1fa5eb404d8020702d15310fd8fd292424a
yash gupta <yash.gupta755@gmail.com>
2021-02-20 19:35:57 +0530

    Data

--- child_category.py
+++ child_category.py
@@ -22,85 +22,89 @@ def get_list():
                         subcategory = data['subcategory']
                         category = data['category']
 
+                        try:
+                            connection1 = pymysql.connect(host="security-supply.com",
+                                                          user="security_ocar590",
+                                                          passwd="27c@44pS]k",
+                                                          db="security_ocar590",
+                                                          charset='utf8mb4',
+                                                          cursorclass=pymysql.cursors.DictCursor)
+
+                            try:
+                                with connection1.cursor() as cursor2:
+                                    sql = "SELECT * FROM oc21_category_description WHERE name = %s"
+                                    adr = subcategory
+
+                                    cursor2.execute(sql, adr)
+                                    connection1.commit()
+
+                                    print (cursor2.rowcount)
+
+                                    if cursor2.rowcount == 0:
+                                        with connection1.cursor() as cursor3:
+                                            sql = "SELECT * FROM oc21_category_description WHERE name = %s"
+                                            adr = category
+
+                                            cursor3.execute(sql, adr)
+                                            connection1.commit()
+
+                                            if cursor3.rowcount >= 1:
+                                                result = cursor3.fetchone()
+                                                print (result)
 
 
 
-                        print (category)
-                        print (subcategory)
+                                        # try:
+                                        #     with connection1.cursor() as cursor3:
+                                        #         cursor3.execute(
+                                        #             "INSERT INTO oc21_category (status, top, sort_order) VALUES (%s,%s,%s)",
+                                        #             (1,1,1))
+                                        #
+                                        #         connection1.commit()
+                                        #
+                                        #         print(cursor3.lastrowid, "record inserted.")
+                                        #         print
+                                        #
+                                        #
+                                        #         # result2 = cursor3.fetchone()
+                                        #         category_id = cursor3.lastrowid
+                                        #         print (category_id)
+                                        #         with connection1.cursor() as cursor4:
+                                        #             cursor4.execute(
+                                        #                 "INSERT INTO  oc21_category_description (name,category_id,language_id) VALUES (%s,%s,%s)",
+                                        #                 (str(category), category_id, 1))
+                                        #
+                                        #         connection1.commit()
+                                        #
+                                        #         with connection1.cursor() as cursor5:
+                                        #             cursor5.execute(
+                                        #                 "INSERT INTO  oc21_category_to_store (store_id,category_id) VALUES (%s,%s)",
+                                        #                 (0, category_id))
+                                        #
+                                        #         connection1.commit()
+                                        #
+                                        #
+                                        #
+                                        #
+                                        #
+                                        # except Exception as e:
+                                        #     print ('Failed Query')
+                                        #     print (e)
 
-                        # try:
-                        #     connection1 = pymysql.connect(host="security-supply.com",
-                        #                                   user="security_ocar590",
-                        #                                   passwd="27c@44pS]k",
-                        #                                   db="security_ocar590",
-                        #                                   charset='utf8mb4',
-                        #                                   cursorclass=pymysql.cursors.DictCursor)
-                        #
-                        #     try:
-                        #         with connection1.cursor() as cursor2:
-                        #             sql = "SELECT * FROM oc21_category_description WHERE name = %s"
-                        #             adr = category
-                        #
-                        #             cursor2.execute(sql, adr)
-                        #             connection1.commit()
-                        #
-                        #             print (cursor2.rowcount)
-                        #
-                        #             print (cursor2.fetchone())
-                        #
-                        #             if cursor2.rowcount == 0:
-                        #                 try:
-                        #                     with connection1.cursor() as cursor3:
-                        #                         cursor3.execute(
-                        #                             "INSERT INTO oc21_category (status, top, sort_order) VALUES (%s,%s,%s)",
-                        #                             (1,1,1))
-                        #
-                        #                         connection1.commit()
-                        #
-                        #                         print(cursor3.lastrowid, "record inserted.")
-                        #                         print
-                        #
-                        #
-                        #                         # result2 = cursor3.fetchone()
-                        #                         category_id = cursor3.lastrowid
-                        #                         print (category_id)
-                        #                         with connection1.cursor() as cursor4:
-                        #                             cursor4.execute(
-                        #                                 "INSERT INTO  oc21_category_description (name,category_id,language_id) VALUES (%s,%s,%s)",
-                        #                                 (str(category), category_id, 1))
-                        #
-                        #                         connection1.commit()
-                        #
-                        #                         with connection1.cursor() as cursor5:
-                        #                             cursor5.execute(
-                        #                                 "INSERT INTO  oc21_category_to_store (store_id,category_id) VALUES (%s,%s)",
-                        #                                 (0, category_id))
-                        #
-                        #                         connection1.commit()
-                        #
-                        #
-                        #
-                        #
-                        #
-                        #                 except Exception as e:
-                        #                     print ('Failed Query')
-                        #                     print (e)
-                        #
-                        #
-                        #             connection1.commit()
-                        #
-                        #
-                        #
-                        #
-                        #     except Exception as e:
-                        #         print ('Failed Query')
-                        #         print (e)
-                        #
-                        # except pymysql.Error as e:
-                        #     print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
-                        #     print ("Loop1")
-                        #     time.sleep(2)
-                        #     print("Was a nice sleep, now let me continue...")
+
+
+
+
+
+                            except Exception as e:
+                                print ('Failed Query')
+                                print (e)
+
+                        except pymysql.Error as e:
+                            print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+                            print ("Loop1")
+                            time.sleep(2)
+                            print("Was a nice sleep, now let me continue...")
 
 
 
