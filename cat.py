dd67114802770b8df3599e2c49b1ab59117ea90f
yash gupta <yash.gupta755@gmail.com>
2021-02-20 18:12:24 +0530

    Data

--- cat.py
+++ cat.py
@@ -3,28 +3,28 @@ import time
 
 
 def get_list():
-    try:
-        connection = pymysql.connect(host="dock14.com",
-                                     user="dockonef_jashg",
-                                     passwd="1uWSEW#pcZcJ",
-                                     db="dockonef_scrape",
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM adi_category GROUP BY category")
-                connection.commit()
-
-
-                for row in cursor:
-                    try:
-                        data = row
-                        category = data['category']
-
-
-
-
-                        print (category)
+    # try:
+    #     connection = pymysql.connect(host="dock14.com",
+    #                                  user="dockonef_jashg",
+    #                                  passwd="1uWSEW#pcZcJ",
+    #                                  db="dockonef_scrape",
+    #                                  charset='utf8mb4',
+    #                                  cursorclass=pymysql.cursors.DictCursor)
+    #     try:
+    #         with connection.cursor() as cursor:
+    #             cursor.execute("SELECT * FROM adi_category GROUP BY category")
+    #             connection.commit()
+    #
+    #
+    #             for row in cursor:
+    #                 try:
+    #                     data = row
+    #                     category = data['category']
+    #
+    #
+    #
+    #
+    #                     print (category)
 
                         # try:
                         #     connection1 = pymysql.connect(host="security-supply.com",
@@ -91,20 +91,20 @@ def get_list():
                         #     print("Was a nice sleep, now let me continue...")
 
 
-
-                    except Exception as e:
-                        print ("Loop3")
-                        print (e)
-                        continue
-        except Exception as e:
-            print ('Failed Query')
-            print ("Loop2")
-            print (e)
-    except pymysql.Error as e:
-        print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
-        print ("Loop1")
-        time.sleep(2)
-        print("Was a nice sleep, now let me continue...")
+    #
+    #                 except Exception as e:
+    #                     print ("Loop3")
+    #                     print (e)
+    #                     continue
+    #     except Exception as e:
+    #         print ('Failed Query')
+    #         print ("Loop2")
+    #         print (e)
+    # except pymysql.Error as e:
+    #     print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+    #     print ("Loop1")
+    #     time.sleep(2)
+    #     print("Was a nice sleep, now let me continue...")
 
     try:
         connection1 = pymysql.connect(host="security-supply.com",
