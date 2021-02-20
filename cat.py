08d7d2382b159da7a74f71a7056f084d8633ecab
yash gupta <yash.gupta755@gmail.com>
2021-02-20 13:09:32 +0530

    Data

--- cat.py
+++ cat.py
@@ -24,21 +24,6 @@ def get_list():
 
                         print (category)
 
-                        try:
-                            connection1 = pymysql.connect(host="security-supply.com",
-                                                         user="security_ocar590",
-                                                         passwd="27c@44pS]k",
-                                                         db="security_ocar590",
-                                                         charset='utf8mb4',
-                                                         cursorclass=pymysql.cursors.DictCursor)
-
-                            print (connection1)
-
-                        except pymysql.Error as e:
-                            print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
-                            print ("Loop1")
-                            time.sleep(2)
-                            print("Was a nice sleep, now let me continue...")
 
 
                     except Exception as e:
@@ -49,6 +34,38 @@ def get_list():
             print ('Failed Query')
             print ("Loop2")
             print (e)
+    except pymysql.Error as e:
+        print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+        print ("Loop1")
+        time.sleep(2)
+        print("Was a nice sleep, now let me continue...")
+
+    try:
+        connection1 = pymysql.connect(host="security-supply.com",
+                                      user="security_ocar590",
+                                      passwd="27c@44pS]k",
+                                      db="security_ocar590",
+                                      charset='utf8mb4',
+                                      cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with connection1.cursor() as cursor2:
+                cursor2.execute(
+                    "INSERT INTO oc21_category (status, top, sort_order) VALUES (%s,%s,%s)",
+                    (1,1,1))
+                connection1.commit()
+
+                result = cursor2.fetchone()
+
+                print (result)
+
+                print (result['insert_id'])
+
+                print ("Inserted")
+        except Exception as e:
+            print ('Failed Query')
+            print (e)
+
     except pymysql.Error as e:
         print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
         print ("Loop1")
