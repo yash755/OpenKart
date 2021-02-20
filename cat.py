7c0866a39baa5b8caaa6267e84ad76f2a1123676
yash gupta <yash.gupta755@gmail.com>
2021-02-20 13:00:20 +0530

    Data

--- cat.py
+++ cat.py
@@ -24,6 +24,22 @@ def get_list():
 
                         print (category)
 
+                        try:
+                            connection1 = pymysql.connect(host="security-supply.com",
+                                                         user="security_ocar590",
+                                                         passwd="27c@44pS]k",
+                                                         db="security_ocar590",
+                                                         charset='utf8mb4',
+                                                         cursorclass=pymysql.cursors.DictCursor)
+
+                            print (connection1)
+
+                        except pymysql.Error as e:
+                            print ("ERROR %d IN CONNECTION: %s" % (e.args[0], e.args[1]))
+                            print ("Loop1")
+                            time.sleep(2)
+                            print("Was a nice sleep, now let me continue...")
+
 
                     except Exception as e:
                         print ("Loop3")
