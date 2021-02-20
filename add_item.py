9a853bb026ae26cdac10e804be1122bd5a38721f
yash gupta <yash.gupta755@gmail.com>
2021-02-20 20:42:01 +0530

    Data

--- add_item.py
+++ add_item.py
@@ -12,14 +12,38 @@ def get_list():
                                      cursorclass=pymysql.cursors.DictCursor)
         try:
             with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM adi LIMIT 10")
+                cursor.execute("SELECT * FROM adi_category GROUP BY subcategory LIMIT 2")
+                # cursor.execute("SELECT * FROM adi WHERE subcategory IS NOT NULL LIMIT 10")
                 connection.commit()
 
 
                 for row in cursor:
                     try:
                         data = row
-                        print (data)
+                        subcategory = data['subcategory']
+                        category = data['category']
+
+                        print (subcategory + '\n\n')
+
+                        with connection.cursor() as cursor1:
+                            sql = "SELECT * FROM adi WHERE subcategory  = %s IS NOT NULL LIMIT 10"
+                            adr = subcategory
+
+                            cursor1.execute(sql, adr)
+                            connection.commit()
+
+                            for row in cursor1:
+                                try:
+                                    data = row
+                                    print (data)
+
+
+                                except Exception as e:
+                                    print ('Failed Query')
+                                    print ("Loop2")
+                                    print (e)
+
+
 
 
 
